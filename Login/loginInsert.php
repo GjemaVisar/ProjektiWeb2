4587f09185ac77daa5afc3f220da83a682a9275d
rron sherifi <rronsherifi48@gmail.com>
2023-04-30 19:45:21 +0200

    add enter to index

--- Login/loginInsert.php
+++ Login/loginInsert.php
@@ -8,8 +8,12 @@
             $check = mysqli_query($conn,"SELECT * FROM user WHERE 
                     email='".$email."' and password='".$password."' ");
 
+
+
             if(mysqli_num_rows($check)==1){
-                echo "Login success";
+                #echo "Login success";
+                header("Location: http://localhost/ProjektiWeb2/", TRUE, 301);
+                exit();
             }else{
                 echo "Login failed";
             }
