29e8c84ec0bed1f649008e14758b77f4760ba998
rron sherifi <rronsherifi48@gmail.com>
2023-04-30 21:44:50 +0200

    add conditions for signup

--- Login/signupInsert.php
+++ Login/signupInsert.php
@@ -8,13 +8,43 @@
             $cfpassword = $_POST['cfpassword'];
             $data = date('d/m/Y');
             
-            if($password == $cfpassword){
+            $u = "SELECT name from user where name = '$name'";
+            $u_query = mysqli_query($conn,$u);
+            
+            $e = "SELECT email from user where email ='$email'";
+            $e_query = mysqli_query($conn,$e);
+            
+            $errors = array();
+            if(empty($name)){
+                $errors['u'] = "Username required";
+
+            }else if(mysqli_num_rows($u_query)>0 ){
+                $errors['u'] = "Username exists";
+
+            }else if(empty($email)){
+                $errors['e'] = "Email required";
+
+            }else if(mysqli_num_rows($e_query)>0){
+                $errors['e'] = "Email exists";
+            }
+            else if(empty($password)){
+                $errors['p'] = "Password required";
+
+            }else if(empty($cfpassword)){
+                $errors['cp'] = "Repeat password please";
+            }
+           
+            if($password == $cfpassword && count($errors)==0){
                 $salt = 'salt@_hellosalt';
                 $hashed = hash('sha256',$password.$salt);
+
+                //check if username exists
+                
 
                 if($insert = mysqli_query($conn,"INSERT INTO user(name,email,password,date) 
                 VALUES ('$name','$email','$hashed','$data')")){
                     #echo "Te dhenat u regj me sukses! ";
+
                     header("Location: http://localhost/ProjektiWeb2/Login/login.php", TRUE, 301);
                     exit();
                 }else{
@@ -26,5 +56,6 @@
         }
         mysqli_close($conn);
     
+#<p class='text-danger'><?php  if(isset($errors['e'])){echo $errors['e'];} / </p>
+?>
 
-?> 
