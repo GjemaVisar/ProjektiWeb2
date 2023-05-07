2c89a3aa0426c241feaed75eeffbf5d1ce12ce6b
rron sherifi <rronsherifi48@gmail.com>
2023-05-07 15:24:15 +0200

    change signup

--- signup.php
+++ signup.php
@@ -14,7 +14,20 @@
 
 		return $salt;
 	}
-
+	
+		//created salt generating function with length 10
+		$length = 10;
+		function generate_salt($length){
+			$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			$salt = '';
+	
+			for($i=0;$i<$length;$i++){
+				$index = rand(0,strlen($chars)-1);
+				$salt .= $chars[$index];
+			}
+	
+			return $salt;
+		}
         if(isset($_POST['submit'])){
             $name = $_POST['name'];
             $email = $_POST['email'];
@@ -40,6 +53,7 @@
             }
            
             else if($password == $cfpassword){
+                $salt = generate_salt($length);
                 $salt = generate_salt($length);
                 $hashed = hash('sha256',$password.$salt);
 
