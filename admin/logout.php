4363e071c09b1aff40a1892fd750f0cc6428aec4
Reis Stanovci <stanovcireis@gmail.com>
2023-05-18 21:38:15 +0200

    tr

--- admin/logout.php
+++ admin/logout.php
@@ -1,8 +1,7 @@
-<?php 
-    session_start(); //to ensure you are using same session
-    session_unset();
-    session_destroy(); //destroy the session
-    header("location:../login.php"); //to redirect back to "index.php" after logging out
+<?php
+    session_start(); // to ensure you are using the same session
+    session_unset(); // remove all session variables
+    session_destroy(); // destroy the session
+    header("location:../login.php");
     exit();
-
 ?>
